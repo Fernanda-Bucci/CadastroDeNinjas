7b056c3e2c26c0d2a0f68e0d60eadb48af23ab4a
Fernanda-Bucci <fernanda.bucci@hotmail.com>
2026-03-10 19:07:24 -0300

    feat: Coloca o lombok na classe para abstração

--- src/main/java/br/com/dev/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/br/com/dev/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,58 +2,28 @@ package br.com.dev.CadastroDeNinjas.Missoes;
 
 import br.com.dev.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nomeMissao;
     private String dificuldade;
 
 
     //OneToMany > uma missão poderá ter vários ninjas
-    @OneToMany (mappedBy = "missoes")
+    @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nomeMissao, String dificuldade, List<NinjaModel> ninjas) {
-        this.nomeMissao = nomeMissao;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
 }
-
-
-
--- src/main/java/br/com/dev/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/br/com/dev/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -3,11 +3,17 @@ package br.com.dev.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import br.com.dev.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -15,44 +21,11 @@ public class NinjaModel {
     private Long id;
     private String nome;
     private String email;
-    private int  idade;
+    private int idade;
 
     //ManyToOne > um ninja tem uma única missão
     @ManyToOne
     @JoinColumn(name = "missoes_id")//chave estrangeira [Foreing Key]
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
