3a2e775b0d6f0332111153be30ab38fe3d37d2f2
Fernanda-Bucci <fernanda.bucci@hotmail.com>
2026-04-06 20:55:20 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependência do service e do Repository

--- src/main/java/br/com/dev/CadastroDeNinjas/Ninjas/Controller/Service/NinjaService.java
+++ src/main/java/br/com/dev/CadastroDeNinjas/Ninjas/Controller/Service/NinjaService.java
@@ -1,4 +1,19 @@
 package br.com.dev.CadastroDeNinjas.Ninjas.Controller.Service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+
+    }
 }
